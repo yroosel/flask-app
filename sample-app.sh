83a5b61e16f63ca4c88455fcb764233cb04fc2e1
yroosel <yvan.rooseleer@biasc.be>
2020-11-09 15:22:56 +0000

    Changed port to 5058

--- sample-app.sh
+++ sample-app.sh
@@ -8,15 +8,15 @@ cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
-echo "FROM python" >> tempdir/Dockerfile
+echo "FROM python" > tempdir/Dockerfile
 echo "RUN pip install flask" >> tempdir/Dockerfile
 echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
 echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
-echo "EXPOSE 5055" >> tempdir/Dockerfile
+echo "EXPOSE 5058" >> tempdir/Dockerfile
 echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
 
 cd tempdir
 docker build -t sampleapp .
-docker run -t -d -p 5055:5055 --name samplerunning sampleapp
+docker run -t -d -p 5058:5058 --name samplerunning sampleapp
 docker ps -a 
